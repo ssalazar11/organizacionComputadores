34984a75f34ec2362cee89d53ca350853ffed3a2
samuel <samsala32@gmail.com>
2023-05-25 01:34:43 -0500

    ciclo añadido pero no funcional

--- proyecto.py
+++ proyecto.py
@@ -1,8 +1,14 @@
 from pydub import AudioSegment
 
 audioUno=AudioSegment.from_file("archivos/Kendrick_Lamar_HUMBLE_.mp3", format="mp3")
-audioDos=AudioSegment.from_file("archivos/FEEL.mp3", format="mp3")
+audioDos=AudioSegment.from_file("archivos/FEEL_.mp3", format="mp3")
 
 audioData=audioUno.get_array_of_samples()
+audioDataDos=audioDos.get_array_of_samples()
 
-print(len(audioData))
+for i in range(len(audioDos)):
+    f=i*1000
+    resultado=audioUno[:f]+audioDos[:f]
+
+
+resultado.export("resultado.mp3", format="mp3")
